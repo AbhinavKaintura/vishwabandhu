1035ab941dbc2b2bc4d2a3148fd6071de655b1e5
AbhinavKaintura <abhinavkaintura001@gmail.com>
2025-01-25 12:36:31 +0530

    members list

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -1,3 +1,4 @@
+
 import React from 'react';
 import HeaderBar from '@/components/common/header-bar/page';
 import NavBar from '@/components/common/nav-bar/page';
@@ -12,10 +13,6 @@ interface Member {
   socialLinks: {
     platform: string;
     url: string;
-    facebook: string;
-    pinterest: string;
-    linkedin: string;
-    twitter: string;
   }[];
 }
 
@@ -23,14 +20,72 @@ interface TeamsProps {
   members: Member[];
 }
 
+const dummyMembers: Member[] = [
+  {
+    id: '1',
+    name: 'Abhinav',
+    image: '/user1.jpg', // Assuming images are served from a static folder
+    socialLinks: [
+      { platform: 'facebook', url: 'https://facebook.com' },
+      { platform: 'pinterest', url: 'https://pinterest.com' },
+      { platform: 'linkedin', url: 'https://linkedin.com' },
+      { platform: 'twitter', url: 'https://twitter.com' },
+    ],
+  },
+  {
+    id: '2',
+    name: 'Donor Lorem',
+    image: '/user1.jpg', // Assuming images are served from a static folder
+    socialLinks: [
+      { platform: 'facebook', url: 'https://facebook.com' },
+      { platform: 'pinterest', url: 'https://pinterest.com' },
+      { platform: 'linkedin', url: 'https://linkedin.com' },
+      { platform: 'twitter', url: 'https://twitter.com' },
+    ],
+  },
+  {
+    id: '3',
+    name: 'Donor Lorem',
+    image: '/user1.jpg', // Assuming images are served from a static folder
+    socialLinks: [
+      { platform: 'facebook', url: 'https://facebook.com' },
+      { platform: 'pinterest', url: 'https://pinterest.com' },
+      { platform: 'linkedin', url: 'https://linkedin.com' },
+      { platform: 'twitter', url: 'https://twitter.com' },
+    ],
+  },
+  {
+    id: '4',
+    name: 'Donor Lorem',
+    image: '/user1.jpg', // Assuming images are served from a static folder
+    socialLinks: [
+      { platform: 'facebook', url: 'https://facebook.com' },
+      { platform: 'pinterest', url: 'https://pinterest.com' },
+      { platform: 'linkedin', url: 'https://linkedin.com' },
+      { platform: 'twitter', url: 'https://twitter.com' },
+    ],
+  },
+  {
+    id: '5',
+    name: 'Donor Lorem',
+    image: '/user1.jpg', // Assuming images are served from a static folder
+    socialLinks: [
+      { platform: 'facebook', url: 'https://facebook.com' },
+      { platform: 'pinterest', url: 'https://pinterest.com' },
+      { platform: 'linkedin', url: 'https://linkedin.com' },
+      { platform: 'twitter', url: 'https://twitter.com' },
+    ],
+  },
+  // Add more members as needed
+];
+
 const Teams: React.FC<TeamsProps> = ({ members }) => {
   return (
     <div className="min-h-screen bg-gray-50">
       <HeaderBar />
       <NavBar bg_color="bg-white" />
       <Title />
-      <MembersList/>
-         
+      <MembersList members={dummyMembers} />
     </div>
   );
 };
