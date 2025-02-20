f90cc2b42ff910a6d45e43607c8ba23f4770a972
Divyanshu Kaintura <divyanshukaintura789@gmail.com>
2025-02-20 21:37:44 +0530

    database migration

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   // Your Firebase config object
-  apiKey: "AIzaSyAfVGu1bfYD0hCRYLxrvzXXak3rBQh4Kjo",
-  authDomain: "vishwa-test-9a48c.firebaseapp.com",
-  projectId: "vishwa-test-9a48c",
-  storageBucket: "vishwa-test-9a48c.firebasestorage.app",
-  messagingSenderId: "616542630330",
-  appId: "1:616542630330:web:9dfe3c4348cfc375642d83"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
